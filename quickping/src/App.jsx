2355cd6033cb1522c474457533451284f06705fa
akshayrajeevnambiar <akshayrajeevnambiar@gmail.com>
2024-09-26 23:21:57 -0400

    Set up file structure

---
+++ quickping/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>QuickPing</h1>
+        <p>Your network speed test tool.</p>
+        <button>Start Test</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
